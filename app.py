ab8a46995770e22b6627766d65e66da9b86f3f34
anu0012 <anurag30671371@gmail.com>
2019-08-18 10:25:06 +0530

    test

--- app.py
+++ app.py
@@ -7,20 +7,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	# use creds to create a client to interact with the Google Drive API
-	scope = ['https://spreadsheets.google.com/feeds',
-	         'https://www.googleapis.com/auth/drive']
-	creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
-	client = gspread.authorize(creds)
+	# # use creds to create a client to interact with the Google Drive API
+	# scope = ['https://spreadsheets.google.com/feeds',
+	#          'https://www.googleapis.com/auth/drive']
+	# creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
+	# client = gspread.authorize(creds)
 
-	# Find a workbook by name and open the first sheet
-	# Make sure you use the right name here.
-	sheet = client.open("Copy of Sample Pair Sheet").sheet1
+	# # Find a workbook by name and open the first sheet
+	# # Make sure you use the right name here.
+	# sheet = client.open("Copy of Sample Pair Sheet").sheet1
 
-	list_of_rows = []
-	# Extract all rows
-	for i in range(sheet.row_count):
-		list_of_rows.append(sheet.row_values(i+1))
+	# list_of_rows = []
+	# # Extract all rows
+	# for i in range(sheet.row_count):
+	# 	list_of_rows.append(sheet.row_values(i+1))
 
 	list_of_rows = [['A','xyz'],['B','pqr']]
 
